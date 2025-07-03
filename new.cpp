7c2d73efed23d74d6cf04b3ce4acff0a9a2f8bf2
Priyanshu1618p <priyanshupatidar924@gmail.com>
2025-07-03 18:32:23 +0530

    table

---
+++ new.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int a;
+     cout<<"Enter a Number: ";
+     cin>>a;
+     cout<<a;
+}
+
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+     int a;
+     cout<<"Enter a Number: ";
+     cin>>a;
+     cout<<a;
+}
