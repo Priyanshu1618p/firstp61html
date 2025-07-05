34ae54d8583ae7be4a6c8f5c2c17cd2a0abcfaf4
Priyanshu1618p <priyanshupatidar924@gmail.com>
2025-07-05 18:31:33 +0530

    css update

--- new.cpp
+++ new.cpp
@@ -1,23 +1,50 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//      int a;
+//      cout<<"Enter a Number: ";
+//      cin>>a;
+//      cout<<a;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//      int a;
+//      cout<<"Enter a Number: ";
+//      cin>>a;
+//      cout<<a;
+// }
 #include <iostream>
 using namespace std;
-
 int main(){
 
-     int a;
-     cout<<"Enter a Number: ";
-     cin>>a;
-     cout<<a;
+     //a=10; //declare
+     //single line comment
+     /*multi line comment*/
+
+     // int a;
+     // cout<<"enter a number: ";
+     // cin>>a;
+     // cout<<"the number you have entered is: "<<a;
+
+     // int a=10;
+     // cout<<a<<endl;
+
+     //  char ='p';
+     // cout<<c<<endl;
+
+     bool d=true;
+     cout<<d;
+
+
+
+
 }
 
-
-
-#include <iostream>
-using namespace std;
-
-int main(){
-
-     int a;
-     cout<<"Enter a Number: ";
-     cin>>a;
-     cout<<a;
-}
